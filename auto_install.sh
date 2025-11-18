38cf61d544740f39ce629d630abdea1e04a3040e
Douglas Hazzea <douglash@ge-j3.kzn.21-school.ru>
2025-11-18 16:07:07 +0300

    write auto_install.sh, testing...

--- auto_install.sh
+++ auto_install.sh
@@ -1,2 +1,57 @@
 #!/bin/bash
+. ./logging.sh
 
+# Проверка прав администратора
+if [ "$EUID" -ne 0 ]; then
+  error "Скрипт должен быть запущен с правами администратора (root)."
+  exit 1
+fi
+
+if [ "$#" -eq 0 ]; then
+  error "Пожалуйста передайте config файл как аргумент!"
+  exit 1
+fi
+
+config=$1
+if [ ! -r "$config" ]; then
+  error "Файл '$config' недоступен или не существует"
+  exit 1
+fi
+
+pids=()
+tmp_files=()
+
+cleanup() {
+  echo "Прерывание: завершаем дочерние процессы..."
+  kill "${pids[@]}" 2>/dev/null
+  wait "${pids[@]}" 2>/dev/null
+  exit 1
+}
+trap cleanup INT TERM
+
+# построчно читает файл - вызывает install.sh
+# передает строку из конфига
+while IFS= read -r line; do
+  tmp=$(mktemp)
+  ./install.sh "$line" >"$tmp" 2>$1 $
+  pids+=($!)
+  tmp_files+=("$tmp")
+done < "$config"
+
+# ждем завершения процессов
+for i in "${!pids[@]}"; do
+  pid=${pids[i]}
+  tmp=${tmp_files[i]}
+
+  wait "$pid"
+  status=$?
+  output=$(cat "$tmp")
+
+  if [ $status -ne 0 ]; then
+    log "$output"
+  else
+    debug "$output"
+  fi
+
+  rm -f "$tmp"
+done
--- logging.sh
+++ logging.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+log_file="log_file.log" 
+
+# args - error_message(уже имеет в форматированный вывод)
+log() {
+  if [ "$#" -eq 0 ]; then
+    echo "Ошибка в функции log: Необходимо передать хотя бы 1 аргумент."
+    return 1
+  fi
+
+  local error_message="$2"
+  local current_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+  {
+    echo "--- $current_time ---"
+    echo "$error_message"
+    echo "--- ^^^^^^^^^^^^^^^^^ ---"
+    echo
+  } >> "$log_file"
+}
+
+debug() {
+  if [ "$#" -eq 0 ]; then
+    echo "Ошибка в функции debug: Необходимо передать хотя бы 1 аргумент."
+    return 1
+  fi
+  echo $1
+}
+
+error() {
+  if [ "$#" -eq 0 ]; then
+    echo "Ошибка в функции error: Необходимо передать хотя бы 1 аргумент."
+    return 1
+  fi
+  echo $1
+}
