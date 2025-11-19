6ad37c765d983f587d1f5edc1ccffa3a4ccb43cb
Douglas Hazzea <douglash@ge-j3.kzn.21-school.ru>
2025-11-19 17:33:04 +0300

    fix

--- install.sh
+++ install.sh
@@ -11,12 +11,11 @@ apt_flags=-y
 is_installed() {
   local name="$1"
   
-  
+  # проверка пакета через dpkg 
   if dpkg -s "$name" >/dev/null 2>&1; then
     return 0
   fi
-
-  # Затем проверим, есть ли исполняемый файл (на случай, если пакет называется иначе, или установлен вручную)
+  # проверка существования бинарника
   if command -v "$name" >/dev/null 2>&1; then
     return 0
   fi
@@ -26,7 +25,7 @@ is_installed() {
 
 download_package_wget() {
   if wget --spider --timeout=5 "$link" >/dev/null 2>&1; then
-    debug "-> Загружаю $app_name..."
+    debug "$app_name: загружаю пакет..."
     wget --timeout=$wget_timeout "$link" -O "$app_name.deb"
     if [ $? -eq 0 ]; then
       debug "$app_name: пакет успешно загружен"
@@ -41,9 +40,10 @@ download_package_wget() {
 }
 
 install_with_apt() {
+  debug "$app_name: Проверка..."
   # если ссылка не указана - пытаемся установить через apt напрямую
   if [ -z "$link" ]; then
-    debug "ссылка не указана, установка через apt"
+    debug "$app_name: ссылка не указана, установка через apt"
     sudo DEBIAN_FRONTEND=noninteractive apt install "$apt_flags" "$app_name"
     if [ $? -eq 0 ]; then
       debug "$app_name: пакет успешно установлен"
@@ -55,6 +55,12 @@ install_with_apt() {
   else
     if download_package_wget; then
       sudo DEBIAN_FRONTEND=noninteractive apt install "$apt_flags" "./$app_name.deb" && rm -f "$app_name.deb"
+      if [ $? -eq 0 ]; then
+      debug "$app_name: пакет успешно установлен"
+      else
+        debug "$app_name: ошибка при установке пакета"
+        return 1
+      fi
     else
       return 1
     fi
@@ -81,7 +87,7 @@ if [ -z "$app_name" ]; then
 fi
 
 if is_installed "$app_name"; then
-  debug "$app_name: уже установлено, пропуск"
+  debug "$app_name: уже установлен, пропуск"
 elif ! install_with_apt; then
   exit 1
 fi
